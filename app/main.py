50d959a50716db045c4ef0fc9a3c0da0e400e20d
kukucorn <hyeonku.dev@gmail.com>
2026-01-04 15:24:39 +0900

    Use only one worker

--- app/main.py
+++ app/main.py
@@ -67,5 +67,6 @@ if __name__ == "__main__":
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=settings.debug
+        reload=settings.debug,
+        workers=1,
     )
